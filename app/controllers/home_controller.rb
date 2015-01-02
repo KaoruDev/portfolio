a86f4b05a4f29e392d21b024f43712a30c184cdd
Kaoru Kohashigawa <dev@kaoruk.com>
2015-01-02 13:34:27 -0600

    Add log out link and redirect user to dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    redirect_to dashboard_path
   end
+
+  def dashboard
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'dashboard' => 'home#dashboard', :as => :dashboard
+
   root to: 'home#index'
 end
