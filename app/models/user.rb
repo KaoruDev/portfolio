bd3beda27cba6d2a4f730bff127683e7dbe2be47
Kaoru Kohashigawa <dev@kaoruk.com>
2015-01-02 14:44:45 -0600

    begin to add posts works flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :validatable
 
   has_many :posts
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :posts
+
   get 'dashboard' => 'blog#dashboard', :as => :dashboard
 
   root to: 'home#index'
