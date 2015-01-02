3aaebf5f1ea7c38788a8a1b5b0f01904d405e647
Kaoru Kohashigawa <dev@kaoruk.com>
2015-01-02 13:49:01 -0600

    Setup blog controller and have it take care of the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'dashboard' => 'home#dashboard', :as => :dashboard
+  get 'dashboard' => 'blog#dashboard', :as => :dashboard
 
   root to: 'home#index'
 end
