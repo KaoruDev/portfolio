4fa7653885303cc85074e6111ed8ff3c22e9f505
Kaoru Kohashigawa <dev@kaoruk.com>
2015-01-17 13:36:51 -0600

    Only show unpublished / final posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,9 +11,9 @@ class HomeController < ApplicationController
   private
 
   def post_tags
-    posts = Post.includes(:tags).order("posts.created_at desc")
+    posts = Post.order("created_at desc").where("(published_at is NULL or published_at < ?) and draft != true", Time.now)
     posts = posts.paginate(page: params[:page] || 1, per_page: 5)
-    posts = posts.where(:tags => {name: params[:tags].split(' ')}) if params[:tags]
+    posts = posts.includes(:tags).where(:tags => {name: params[:tags].split(' ')}) if params[:tags]
     posts
   end
 
