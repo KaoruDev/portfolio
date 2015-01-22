9af1c8e75629e0444455830b9294dd1d3ce0325b
Kaoru Kohashigawa <kaoru@spredfast.com>
2015-01-22 08:49:28 -0600

    Fix bug on tag search.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   private
 
   def post_tags
-    posts = Post.order("created_at desc").where("(published_at is NULL or published_at < ?) and draft is not true", Time.now)
+    posts = Post.order("posts.created_at desc").where("(published_at is NULL or published_at < ?) and draft is not true", Time.now)
     posts = posts.paginate(page: params[:page] || 1, per_page: 5)
     posts = posts.includes(:tags).where(:tags => {name: params[:tags].split(' ')}) if params[:tags]
     posts
