9615f62b8cbc98e49ccdfc89af1435843fc36fb6
Kaoru Kohashigawa <dev@kaoruk.com>
2015-02-01 18:42:21 -0600

    add home specs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = post_tags
+    @posts = list_of_posts
   end
 
   def about
@@ -9,11 +9,15 @@ class HomeController < ApplicationController
 
   private
 
-  def post_tags
+  def list_of_posts
     posts = Post.order("posts.created_at desc").where("(published_at is NULL or published_at < ?) and draft is not true", Time.now)
     posts = posts.paginate(page: params[:page] || 1, per_page: 5)
-    posts = posts.includes(:tags).where(:tags => {name: params[:tags].split(' ')}) if params[:tags]
+    posts = filter_by_tags(posts) if params[:tags]
     posts
+  end
+
+  def filter_by_tags(posts)
+    posts.where(id: Tag.where(name: params[:tags], taggable_type: "Post").pluck(:taggable_id))
   end
 
 end
