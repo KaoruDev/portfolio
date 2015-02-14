64726fa5171af644c7545c46c2acb1fd528912f7
Kaoru Kohashigawa <dev@kaoruk.com>
2015-02-13 20:23:33 -0600

    Add a simple published at test for post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Post do
+RSpec.describe Post do 
+  describe '#set_published_at' do
+    it 'will set to a time' do
+      post = Post.new(author: Fabricate(:user), title: "Fake title", body: "fakey fake fake")
+      post.save
+
+      expect(post.published_at).not_to be_nil
+    end
+  end
 end
