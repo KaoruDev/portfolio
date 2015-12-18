13397f54904bf2c4c6073a9b9d7008949dfc2643
Kaoru Kohashigawa <dev@kaoruk.com>
2015-12-18 07:51:06 -0800

    create list of winning combinations

--- lib/projects/connect_four.rb
+++ lib/projects/connect_four.rb
@@ -1,6 +1,58 @@
 module Projects
   class ConnectFour
-    NUM_COLUMNS = 9
+    NUM_COLUMNS = 7
     NUM_ROWS = 6
+    NUM_TO_WIN = 4
+
+    WINNING_COMBINATIONS = Array.new
+
+    # VERTICAL WINS ==============================
+    (NUM_COLUMNS).times do |column|
+      (0..(NUM_ROWS - NUM_TO_WIN)).each do |edge|
+        combo = []
+        (edge..(edge + NUM_TO_WIN - 1)).each do |row|
+          combo << [column, row]
+        end
+        WINNING_COMBINATIONS << combo
+      end
+    end
+
+    # HORIZONTAL WINS ==============================
+    (NUM_ROWS).times do |row|
+      (0..(NUM_COLUMNS - NUM_TO_WIN)).each do |edge|
+        combo = []
+        (edge..(edge + NUM_TO_WIN - 1)).each do |column|
+          combo << [column, row]
+        end
+        WINNING_COMBINATIONS << combo
+      end
+    end
+
+    # FORWARD (/) DIAGONAL WINS ==============================
+
+    (NUM_TO_WIN - 1).times do |edge|
+      ((NUM_TO_WIN - 1)...NUM_COLUMNS).each do |column|
+        combo = []
+        (edge...(edge + NUM_TO_WIN)).each_with_index do |row, idx|
+          combo << [column - idx, row]
+        end
+        WINNING_COMBINATIONS << combo
+      end
+    end
+
+    # BACKWARD (\) DIAGONAL WINS ==============================
+
+    (0..(NUM_COLUMNS - NUM_TO_WIN)).each do |column|
+      (NUM_TO_WIN - 1).times do |row|
+        combo = []
+        4.times do |edge|
+          combo << [column + edge, row + edge]
+        end
+        WINNING_COMBINATIONS << combo
+      end
+    end
+
+    (0...NUM_TO_WIN).each do |edge|
+    end
   end
 end
